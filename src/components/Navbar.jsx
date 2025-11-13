f921f738e9feec819e20958c1565257982a0801b
Zenia Villa <zeniavilla@hotmail.com>
2025-11-12 21:06:52 -0800

    Update favicon ; add navbar right links

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -6,20 +6,23 @@ import {
   NavigationMenuList,
 } from "@/components/ui/NavigationMenu";
 import { ThemeToggle } from "@/components/ThemeToggle";
-import {Activity, Code} from "lucide-react";
+import {Activity, Code, Download} from "lucide-react";
+import {Button} from "@/components/ui/button";
+import {LinkedInIcon} from "@/components/icons";
+import Resume from "../../public/files/andre-merten-resume.pdf";
 
 export function Navbar() {
   const location = useLocation();
 
   return (
     <nav className="border-b">
-      <div className="container mx-auto px-4 py-4">
+      <div className="w-[90%] mx-auto py-4">
         <div className="flex items-center justify-between">
           <NavigationMenu>
             <NavigationMenuList className="gap-3">
               <NavigationMenuItem>
                 <NavigationMenuLink asChild>
-                  <Link to="/overview" className={`flex flex-row gap-2 items-center rounded-lg whitespace-nowrap transition-all ${
+                  <Link to="/overview" className={`flex flex-row gap-2 items-center px-4 py-3 rounded-lg whitespace-nowrap transition-all ${
                     location.pathname === "/overview"
                       ? "bg-blue-600 text-white shadow-lg focus:bg-blue-600"
                       : "bg-slate-800 text-slate-300 hover:bg-slate-700"
@@ -32,7 +35,7 @@ export function Navbar() {
 
               <NavigationMenuItem>
                 <NavigationMenuLink asChild>
-                  <Link to="/resume" className={`flex flex-row gap-2 items-center rounded-lg whitespace-nowrap transition-all ${
+                  <Link to="/resume" className={`flex flex-row gap-2 items-center px-4 py-3 rounded-lg whitespace-nowrap transition-all ${
                     location.pathname === "/resume"
                       ? "bg-blue-600 text-white shadow-lg focus:bg-blue-600"
                       : "bg-slate-800 text-slate-300 hover:bg-slate-700"
@@ -44,7 +47,21 @@ export function Navbar() {
               </NavigationMenuItem>
             </NavigationMenuList>
           </NavigationMenu>
-          {/*<ThemeToggle />*/}
+          <div className="flex items-center">
+            <Button asChild variant="ghost" href="/" title="LinkedIn">
+              <a href="https://www.linkedin.com/in/dremer10" target="_blank" rel="noreferrer">
+                <LinkedInIcon />
+                LinkedIn
+              </a>
+            </Button>
+            <Button asChild variant="ghost" href="/">
+              <a href={Resume} download>
+                <Download />
+                Download Resume
+              </a>
+            </Button>
+            {/*<ThemeToggle />*/}
+          </div>
         </div>
       </div>
     </nav>

ccbe808b369b7baac6421cf41c3f3105a8d6979d
Andre Merten <dremer415@gmail.com>
2025-11-18 21:48:21 -0800

    add new animation to resume page, add credential redirect

--- src/data/resume.js
+++ src/data/resume.js
@@ -2,9 +2,9 @@ import {Activity, BookOpenText, CircleUser, MapPin} from "lucide-react";
 
 export const personalInfo = {
   name: "Andreas Merten",
-  role: "DevOps and Test Engineer",
+  role: "DevOps/Infra QA Engineer",
   summary: [
-    "I am an accomplished DevOps and Test Engineer with over seven years of experience deploying, testing, and maintaining robust on-premises and cloud-based applications. I have contributed to seamless migrations of legacy systems, automated infrastructure tasks, and improved operational efficiency through Kubernetes, Docker, and Python scripting. My experience includes developing CI/CD pipelines and managing cloud infrastructure across AWS, Azure, and GCP.",
+    "I am an accomplished DevOps/Infra QA with over seven years of experience deploying, testing, and maintaining robust on-premises and cloud-based applications. I have contributed to seamless migrations of legacy systems, automated infrastructure tasks, and improved operational efficiency through Kubernetes, Docker, and Python scripting. My experience includes developing CI/CD pipelines and managing cloud infrastructure across AWS, Azure, and GCP.",
     "I have practical experience implementing Blue-Green deployment strategies to support seamless releases and minimize downtime. Additionally, I have extensive experience in TLS certificate management, as well as secret management and rotation. I focus on building and designing systems with security integrated from the start, ensuring reliability, scalability, and compliance throughout the development lifecycle."
   ]
 };
@@ -18,11 +18,13 @@ export const metadataInfo = [
 
 export const coreSkills = [
   "Docker",
+  "Kubernetes",
   "Cloud",
   "Shell",
   "Security",
   "Containerization",
   "Infra as Code",
+  "IT Administration",
   "Python",
   "GitLab CI"
 ];
@@ -30,20 +32,43 @@ export const coreSkills = [
 export const experienceData = [
   {
     company: "Aspera, an IBM company",
-    position: "DevOps and Test Engineer (Band 8)",
-    years: "2019 - Present",
+    position: "DevOps/Infra QA",
+    years: "2020 - Present",
+    status: "Active",
     highlights: [
-      "Implemented automation and parallel testing, reducing QA cycle times and accelerating release schedules.",
-      "Key contributor to the release cycles of HSTS, AsperaProxy, HTTP-Gateway, and OTFV (Out-of-file-validation) virus detection ensuring stability and timely delivery.",
-      "Enhanced product performance and customer satisfaction by contributing to Faspex5 versions 5.0.0 through 5.0.14.",
+      "Implemented automation and parallel testing, reducing QA cycle times and\n          accelerating release schedules.",
+      "Key contributor to the release cycles of HSTS, AsperaProxy, HTTP-Gateway,\n          and OTFV (Out-of-file-validation) virus detection ensuring stability and\n          timely delivery.",
+      "Enhanced product performance and customer satisfaction by contributing to\n          Faspex5 versions 5.0.0 through 5.0.14.",
       "Created turnkey test environments, reducing setup time by 50%",
-      "Enabled parallel deployments across all supported OS versions, improving team efficiency.",
-      "Helped Architect and implement high-availability Faspex5 deployments. Responsible for validation testing for both on-prem (MariaDB Galera) and cloud (RDS HA) ensuring compatibility.",
-      "Managed and successfully drove the annual DISA Security STIG testing to completion as the sole engineer for the past five years.",
-      "Coordinated with External Security Teams and Pentest Teams for annual security Testing for entire WebApp Portfolio.",
-      "Developed, maintained, and Dockerized automation scripts to streamline workflows.",
-      "Led AWS cloud deployments and applied CI/CD pipelines using Jenkins for core and legacy web applications.",
-      "Conducted comprehensive validation testing across distributed systems, containers, and cloud infrastructure."
+      "Enabled parallel deployments across all supported OS versions, improving team\n          efficiency.",
+      "Helped Architect and implement high-availability Faspex5 deployments.\n          Responsible for validation testing for both on-prem (MariaDB Galera) and\n          cloud (RDS HA) ensuring compatibility.",
+      "Managed and successfully drove the annual DISA Security STIG testing to\n          completion as the sole engineer for the past five years.",
+      "Coordinated with External Security Teams and Pentest Teams for annual security\n          Testing for entire WebApp Portfolio.",
+      "Developed, maintained, and Dockerized automation scripts to streamline\n          workflows.",
+      "Led AWS cloud deployments and applied CI/CD pipelines using Jenkins for core\n          and legacy web applications.",
+      "Conducted comprehensive validation testing across distributed systems,\n          containers, and cloud infrastructure."
+    ]
+  },
+  {
+    company: "Aspera, an IBM company",
+    position: "Software Apprentice",
+    years: "2019 - 2020",
+    highlights: [
+      "Completed an intensive hands-on program specializing in software development, quality assurance, and testing methodologies.",
+      "Acquired practical experience in deploying, testing, and maintaining enterprise applications in both on-premises and cloud environments.",
+      "Developed foundational skills in DevOps practices, automation, and continuous integration pipelines, establishing a strong entry into the technology field."
+    ]
+  },
+  {
+    company: "Infoset-ng",
+    position: "Open Source Contributor",
+    years: "August 2017 - Feb 2018",
+    highlights: [
+      "Performed Unit Testing, Debugging and Technical Documentation.",
+      "Contributed to the development of an all-purpose data gathering application.",
+      "Collaborated in the design infrastructure and deployment of MySQL based back-end.",
+      "Implemented database connection pooling to reduce the load on the database server when experiencing high transaction volumes.",
+      "Assisted with the set up and maintenance of the organization's network agents"
     ]
   }
 ];
@@ -68,7 +93,6 @@ export const comments = [
   "reducing setup time by 50%"
 ];
 
-// Commented out experience for reference
 export const additionalExperience = [
   {
     company: "IBM Aspera",

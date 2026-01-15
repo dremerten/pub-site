dff168b7f5894a5dbb34e9ab8ee82a66f495c933
Andre Merten <dremer415@gmail.com>
2026-01-15 09:42:57 -0800

    set version for dev and prod

--- src/components/PageWrapper.jsx
+++ src/components/PageWrapper.jsx
@@ -3,7 +3,7 @@ import PageContainer from "./PageContainer.jsx";
 const PageWrapper = ({ children }) => {
   return (
     <PageContainer fullHeight={true}>
-      <div className="w-[95%] md:w-[90%] mx-auto pt-4 md:pt-8 pb-12 md:pb-20">
+      <div className="w-[95%] md:w-[90%] mx-auto pt-4 md:pt-8 pb-12 md:pb-20 relative">
         {children}
       </div>
     </PageContainer>
